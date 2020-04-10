2d362327047f8be812757ed92050263cfdf329de
Sandra Ma <sandra.ma080@gmail.com>
2020-04-10 22:23:31 +1000

    Updated Step5Outcome .java and .xml and Fixed title

--- app/src/main/java/com/example/infs3634groupassignment/MainActivity.java
+++ app/src/main/java/com/example/infs3634groupassignment/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         mButtonSignIn = findViewById(R.id.btnSignIn);
         mButtonSignIn.setOnClickListener(new View.OnClickListener(){
             @Override
--- app/src/main/java/com/example/infs3634groupassignment/Step5Outcome.java
+++ app/src/main/java/com/example/infs3634groupassignment/Step5Outcome.java
@@ -2,14 +2,61 @@ package com.example.infs3634groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Step5Outcome extends AppCompatActivity {
-
+    private ImageView home;
+    private ImageView trophy;
+    private ImageView notebook;
+    private ImageView profile;
+    private ImageView settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step5_outcome);
         setTitle("Step 5: Final Outcome");
+        home = findViewById(R.id.ivHome);
+        trophy= findViewById(R.id.ivTrophy);
+        notebook = findViewById(R.id.ivNotebook);
+        profile = findViewById(R.id.ivProfile);
+        settings = findViewById(R.id.ivSettings);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step5Outcome.this, Home.class));
+            }
+        });
+
+        trophy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step5Outcome.this, Achievement.class));
+            }
+        });
+
+        notebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step5Outcome.this, Notebook.class));
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step5Outcome.this, Profile.class));
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Step5Outcome.this, Settings.class));
+            }
+        });
     }
 }
