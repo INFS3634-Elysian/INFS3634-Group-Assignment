c45ef8be7a7c160f9f1da8ddb7b0cfb0afdb5241
Truman Ng <ng.truman@gmail.com>
2020-04-13 14:03:42 +1000

    Updated Resume Page -included dividers -updated formatting
    Updated Drawables
    -included divider.xml

--- app/src/main/java/com/example/infs3634groupassignment/Step1AppResume.java
+++ app/src/main/java/com/example/infs3634groupassignment/Step1AppResume.java
@@ -18,6 +18,9 @@ public class Step1AppResume extends AppCompatActivity {
     private TextView info1;
     private TextView info2;
     private TextView info3;
+    private TextView title1;
+    private TextView title2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,15 +70,20 @@ public class Step1AppResume extends AppCompatActivity {
         info1 = findViewById(R.id.txvPart1);
         info2 = findViewById(R.id.txvPart2);
         info3 = findViewById(R.id.txvPart3);
+        title1 = findViewById(R.id.txvTitle1);
+        title2 = findViewById(R.id.txvTitle2);
 
         String info1String = "A <b>résumé</b> or resume is a document used and created by a person to present their <b>background, skills, and accomplishments</b>.";
         String info2String = "Résumés can be used for a variety of reasons, but most often they are used to <b>secure new employment</b>.";
         String info3String = "A typical résumé contains a \"summary\" of <b>relevant job experience and education</b>. The résumé is usually one of the first items, along with a cover letter and sometimes an application for employment, which a potential employer sees regarding the job seeker and is <b>typically used to screen applicants</b>, often followed by an interview. ";
+        String title1String = "<b>What is a résumé?</b>";
+        String title2String = "<b>Why do we need résumés?</b>";
 
         info1.setText(Html.fromHtml(info1String));
         info2.setText(Html.fromHtml(info2String));
         info3.setText(Html.fromHtml(info3String));
-
+        title1.setText(Html.fromHtml(title1String));
+        title2.setText(Html.fromHtml(title2String));
 
     }
 }
