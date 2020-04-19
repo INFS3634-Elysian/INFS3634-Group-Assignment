1d8c3cdad0a6d68169fba10c96b61bb419392a8b
Sandrama123 <sandra.ma@student.unsw.edu.au>
2020-04-19 15:24:07 +1000

    Finished Outcome/OutcomeOffer/OutcomeReject pt2

--- app/src/main/java/com/example/infs3634groupassignment/Step6FirstDay.java
+++ app/src/main/java/com/example/infs3634groupassignment/Step6FirstDay.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Step6FirstDay extends AppCompatActivity {
     private ImageView home;
@@ -13,6 +14,14 @@ public class Step6FirstDay extends AppCompatActivity {
     private ImageView notebook;
     private ImageView profile;
     private ImageView settings;
+
+    private ImageView wear;
+    private ImageView bring;
+    private ImageView say;
+    private ImageView other;
+    private TextView finalText;
+    private View divider;
+    private TextView finalText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,17 @@ public class Step6FirstDay extends AppCompatActivity {
         notebook = findViewById(R.id.ivNotebook);
         profile = findViewById(R.id.ivProfile);
         settings = findViewById(R.id.ivSettings);
+
+        wear = (ImageView) findViewById(R.id.ivWear);
+        bring = (ImageView) findViewById(R.id.ivBring);
+        say = (ImageView) findViewById(R.id.ivSay);
+        other = (ImageView) findViewById(R.id.ivOther);
+        finalText= findViewById(R.id.tvFinal);
+        divider = findViewById(R.id.divider);
+        finalText2 = findViewById(R.id.tvFinal2);
+
+        finalText.setVisibility(View.INVISIBLE);
+        divider.setVisibility(View.INVISIBLE);
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +78,59 @@ public class Step6FirstDay extends AppCompatActivity {
                 startActivity(new Intent(Step6FirstDay.this, Settings.class));
             }
         });
+
+        wear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalText.setVisibility(View.VISIBLE);
+                finalText.setText("What to Wear");
+                divider.setVisibility(View.VISIBLE);
+                finalText2.setText("What you wear will strongly depend on your team and the overall culture at your firm. But for most cases, the safest option will tend to be business professional attire. \n"+
+                        "For men, that's a suit and tie \n" +
+                        "For women, that's a blazer and dress shoes");
+
+            }
+        });
+        bring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalText.setVisibility(View.VISIBLE);
+                finalText.setText("What to Bring");
+                divider.setVisibility(View.VISIBLE);
+                finalText2.setText("There is no need to bring a luggage of personal items to your first day at work. Keep it to the essentials and you'll start your new job on the right note. A few of these essentials include:" +
+                        "- The classic notepad and pen\n" +
+                        "- Additional paperwork (if requested)\n" +
+                        "- ID card or other documents that verify your identity\n" +
+                        "- And of course, the right attitude\n");
+            }
+        });
+        say.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalText.setVisibility(View.VISIBLE);
+                finalText.setText("What to Say");
+                divider.setVisibility(View.VISIBLE);
+                finalText2.setText("It is important that you be yourself and be natural when conversing with your team and others at the firm. Remember, your employer's impression of you is who you presented at the interview!" +
+                        "- Prepare a personal speech as you will be meeting many new people \n" +
+                        "- Actively listen so you are able to engage with the conversation\n"+
+                        "- Collate a couple of small talk topics or questions in the case that you are stuck for words because of nerves!");
+            }
+        });
+        other.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalText.setVisibility(View.VISIBLE);
+                finalText.setText("Other");
+                divider.setVisibility(View.VISIBLE);
+                finalText2.setText(
+                        "- Remember the names of those you meet\n" +
+                        "- Grab a coffee with your team and get to know them\n" +
+                        "- Accept that lunch invitation\n" +
+                        "- Keep in contact with other candidates you met during the recruitment process\n"
+                );
+            }
+        });
+
+
     }
 }
