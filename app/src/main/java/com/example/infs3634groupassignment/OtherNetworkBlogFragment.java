687d0934521039405dd87892515f2ddfc35433fc
jasenyu <jasen1999@hotmail.com>
2020-04-24 00:03:34 +1000

    Jasen's commit

--- app/src/main/java/com/example/infs3634groupassignment/OtherNetworkBlogFragment.java
+++ app/src/main/java/com/example/infs3634groupassignment/OtherNetworkBlogFragment.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 public class OtherNetworkBlogFragment extends Fragment {
     private TextView blogText;
     private ImageView resImage;
-    private Button connect1;
+    private Button connect1, connect2, connect3;
 
     public OtherNetworkBlogFragment() {
         // Required empty public constructor
@@ -29,26 +29,44 @@ public class OtherNetworkBlogFragment extends Fragment {
 
     }
 
-    public void openSandra(View view){
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/sandrama2/"));
-        startActivity(browserIntent);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_other_networking_blog, container, false);
+        connect1 = v.findViewById(R.id.connect1);
+        connect1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openSandra();
+            }
 
+        });
 
 
         return v;
 
 
+    }
 
 
 
+    public void openSandra(){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/sandrama2/"));
+        startActivity(browserIntent);
+    }
 
+    public void openTruman(){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/sandrama2/"));
+        startActivity(browserIntent);
+    }
 
+    public void openJasen(){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/sandrama2/"));
+        startActivity(browserIntent);
+    }
+
+    public void openCathy(){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/sandrama2/"));
+        startActivity(browserIntent);
     }
 }
