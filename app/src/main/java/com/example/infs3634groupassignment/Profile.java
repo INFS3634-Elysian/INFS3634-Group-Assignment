e773fca5074a5fb603558e1f9dc1770d1776e994
Sandrama123 <sandra.ma@student.unsw.edu.au>
2020-04-25 16:27:15 +1000

    Create Chuck Norris API function in Profile

--- app/src/main/java/com/example/infs3634groupassignment/Profile.java
+++ app/src/main/java/com/example/infs3634groupassignment/Profile.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.infs3634groupassignment.Entities.Inspire;
+
+import retrofit2.Retrofit;
+import retrofit2.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Profile extends AppCompatActivity {
     private ImageView home;
@@ -17,6 +27,10 @@ public class Profile extends AppCompatActivity {
     private ImageView notebook;
     private ImageView profile;
     private ImageView settings;
+    private Button btnInspire;
+    private TextView tvQuote;
+    private String TAG = "Profile";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +38,37 @@ public class Profile extends AppCompatActivity {
         setTitle("Profile");
 
         home = findViewById(R.id.ivHome);
-        trophy= findViewById(R.id.ivTrophy);
+        trophy = findViewById(R.id.ivTrophy);
         notebook = findViewById(R.id.ivNotebook);
         profile = findViewById(R.id.ivProfile);
         settings = findViewById(R.id.ivSettings);
+        btnInspire = findViewById(R.id.btnInspire);
+        tvQuote = findViewById(R.id.tvQuote);
+
+        // Convert Json response string into a Java object, connect retrofit with Inspire interface and call API to retrieve the joke
+        btnInspire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.chucknorris.io/").addConverterFactory(GsonConverterFactory.create()).build();
+                InspireService service = retrofit.create(InspireService.class);
+                Call<Inspire> inspireCall = service.getValue();
+                inspireCall.enqueue(new Callback<Inspire>() {
+                    @Override
+                    public void onResponse(Call<Inspire> call, Response<Inspire> response) {
+                        Log.d(TAG, "onResponse: SUCCESS");
+                        String joke = response.body().getValue();
+                        tvQuote.setVisibility(View.VISIBLE);
+                        tvQuote.setText("\"" + joke + "\"");
+                        btnInspire.setText("More!");
+                    }
+
+                    @Override
+                    public void onFailure(Call<Inspire> call, Throwable t) {
+                        Log.d(TAG, "onFailure: FAILURE");
+                    }
+                });
+            }
+        });
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,7 +111,7 @@ public class Profile extends AppCompatActivity {
         } else {
             LayoutInflater inflater = getLayoutInflater();
             View view = inflater.inflate(R.layout.toast_layout,
-                    (ViewGroup)findViewById(R.id.relativeLayout1));
+                    (ViewGroup) findViewById(R.id.relativeLayout1));
             ImageView image = view.findViewById(R.id.imvImage);
             image.setImageResource(R.drawable.ach_profile);
             TextView text = view.findViewById(R.id.textView2);
