0d418efe9288126d4b56dfc3a81bdeef4681d533
Sandrama123 <sandra.ma@student.unsw.edu.au>
2020-04-26 09:25:46 +1000

    Add shadow for text in recyclerview. Fix UI

--- app/src/main/java/com/example/infs3634groupassignment/ApplicationDatabase.java
+++ app/src/main/java/com/example/infs3634groupassignment/ApplicationDatabase.java
@@ -31,16 +31,16 @@ public class ApplicationDatabase {
     // Populating the data fields with specific information associated with each restaurant
     public static ArrayList<ApplicationDatabase> getApplicationDatabase(){
         ArrayList<ApplicationDatabase> applicationDatabase = new ArrayList<>();
-        applicationDatabase.add(new ApplicationDatabase("Step 1: Applicaton- Resume","home_resume"));
-        applicationDatabase.add(new ApplicationDatabase("Step 1: Application- CV","home_cv"));
-        applicationDatabase.add(new ApplicationDatabase("Step 2: Psychometric Test- Behavioural","home_behavioural"));
-        applicationDatabase.add(new ApplicationDatabase("Step 2: Psychometric Test- Cognitive","home_cognitive"));
-        applicationDatabase.add(new ApplicationDatabase("Step 2: Psychometric Test- Numerical","home_numerical"));
-        applicationDatabase.add(new ApplicationDatabase("Step 3: Video Interview", "home_video"));
-        applicationDatabase.add(new ApplicationDatabase("Step 4: Assessment Centre","home_groupwork"));
+        applicationDatabase.add(new ApplicationDatabase("Step 1 | Applicaton- Resume","home_resume"));
+        applicationDatabase.add(new ApplicationDatabase("Step 1 | Application- CV","home_cv"));
+        applicationDatabase.add(new ApplicationDatabase("Step 2 | Behavioural Psychometric Test","home_behavioural"));
+        applicationDatabase.add(new ApplicationDatabase("Step 2 | Cognitive Psychometric Test","home_cognitive"));
+        applicationDatabase.add(new ApplicationDatabase("Step 2 | Numerical Psychometric Test","home_numerical"));
+        applicationDatabase.add(new ApplicationDatabase("Step 3 | Video Interview", "home_video"));
+        applicationDatabase.add(new ApplicationDatabase("Step 4 | Assessment Centre","home_groupwork"));
      //   applicationDatabase.add(new ApplicationDatabase("Step 4: Assessment Centre- Interview","home_interview"));
-        applicationDatabase.add(new ApplicationDatabase("Step 5: Final Outcome","home_outcome"));
-        applicationDatabase.add(new ApplicationDatabase("Step 6: First Day of Work", "home_firstday"));
+        applicationDatabase.add(new ApplicationDatabase("Step 5 | Final Outcome","home_outcome"));
+        applicationDatabase.add(new ApplicationDatabase("Step 6 | First Day of Work", "home_firstday"));
         applicationDatabase.add(new ApplicationDatabase("Networking", "home_networking"));
         applicationDatabase.add(new ApplicationDatabase("Career Opportunities","home_career"));
 
