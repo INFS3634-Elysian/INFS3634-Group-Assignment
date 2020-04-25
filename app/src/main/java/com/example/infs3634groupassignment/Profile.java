dbda1b44f29dac97731964648704b12c096fba97
Sandrama123 <sandra.ma@student.unsw.edu.au>
2020-04-26 00:01:36 +1000

    AsyncTask for API call

--- app/src/main/java/com/example/infs3634groupassignment/Profile.java
+++ app/src/main/java/com/example/infs3634groupassignment/Profile.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +13,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.AsyncTask;
 
 import com.example.infs3634groupassignment.Entities.Inspire;
+
+import java.io.IOException;
 
 import retrofit2.Retrofit;
 import retrofit2.Response;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Profile extends AppCompatActivity {
@@ -56,51 +57,61 @@ public class Profile extends AppCompatActivity {
         btnInspire = findViewById(R.id.btnInspire);
         tvQuote = findViewById(R.id.tvQuote);
 
+        btnInspire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            new GetQuoteTask().execute();
+            }
+        });
+
 
         // Functionality for saving goal into sharedPreferences file
         savedGoal = getSharedPreferences("goal", MODE_PRIVATE);
-        goal.setText((savedGoal.getString("tag","")));
+        goal.setText((savedGoal.getString("tag", "")));
         btnSave.setOnClickListener(new View.OnClickListener() {
-                                       @Override
-                                       public void onClick(View view) {
-                                           if (goal.getText().toString() != null) {
-                                               makeTag(goal.getText().toString());
-                                               Toast.makeText(getApplicationContext(), "Your changes have been saved!", Toast.LENGTH_LONG).show();
-                                           } else if (goal.getText().toString().length() == 0)  {
-                                               Toast.makeText(getApplicationContext(), "There is nothing to save!", Toast.LENGTH_LONG).show();
-                                           } else {
-                                               Toast.makeText(getApplicationContext(), "Your changes have been saved!", Toast.LENGTH_LONG).show();
-                                           }
-                                       }
-                                   });
-
+            @Override
+            public void onClick(View view) {
+                if (goal.getText().toString() != null) {
+                    makeTag(goal.getText().toString());
+                    Toast.makeText(getApplicationContext(), "Your changes have been saved!", Toast.LENGTH_LONG).show();
+                } else if (goal.getText().toString().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "There is nothing to save!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Your changes have been saved!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
 
         // Functionality for the inspirational quote
         // Convert Json response string into a Java object, connect retrofit with Inspire interface and call API to retrieve the joke
-        btnInspire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.chucknorris.io/").addConverterFactory(GsonConverterFactory.create()).build();
-                InspireService service = retrofit.create(InspireService.class);
-                Call<Inspire> inspireCall = service.getValue();
-                inspireCall.enqueue(new Callback<Inspire>() {
-                    @Override
-                    public void onResponse(Call<Inspire> call, Response<Inspire> response) {
-                        Log.d(TAG, "onResponse: SUCCESS");
-                        String joke = response.body().getValue();
-                        tvQuote.setVisibility(View.VISIBLE);
-                        tvQuote.setText("\"" + joke + "\"");
-                        btnInspire.setText("More!");
-                    }
+//        btnInspire.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Retrofit retrofit = new Retrofit.Builder()
+//                        .baseUrl("https://api.chucknorris.io/")
+//                        .addConverterFactory(GsonConverterFactory.create())
+//                        .build();
+//                InspireService service = retrofit.create(InspireService.class);
+//                Call<Inspire> inspireCall = service.getValue();
+//                inspireCall.enqueue(new Callback<Inspire>() {
+//                    @Override
+//                    public void onResponse(Call<Inspire> call, Response<Inspire> response) {
+//                        Log.d(TAG, "onResponse: SUCCESS");
+//                        String joke = response.body().getValue();
+//                        tvQuote.setVisibility(View.VISIBLE);
+//                        tvQuote.setText("\"" + joke + "\"");
+//                        btnInspire.setText("More!");
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<Inspire> call, Throwable t) {
+//                        Log.d(TAG, "onFailure: FAILURE");
+//                    }
+//                });
+//            }
+//        });
 
-                    @Override
-                    public void onFailure(Call<Inspire> call, Throwable t) {
-                        Log.d(TAG, "onFailure: FAILURE");
-                    }
-                });
-            }
-        });
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -156,11 +167,40 @@ public class Profile extends AppCompatActivity {
             databaseHelper.setAchievementStatus("Who am I?");
         }
     }
-    private void makeTag(String tag){
+
+
+    private void makeTag(String tag) {
         String head = savedGoal.getString(tag, "");
         SharedPreferences.Editor preferencesEditorHeading = savedGoal.edit();
-        preferencesEditorHeading.putString("tag",tag);
+        preferencesEditorHeading.putString("tag", tag);
         preferencesEditorHeading.commit();
+    }
+
+    private class GetQuoteTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://api.chucknorris.io/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                InspireService service = retrofit.create(InspireService.class);
+                Call<Inspire> inspiresCall = service.getValue();
+                Response<Inspire> inspiresResponse = inspiresCall.execute();
+                String inspires = inspiresResponse.body().getValue();
+                return inspires;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String inspire) {
+                        tvQuote.setVisibility(View.VISIBLE);
+                        tvQuote.setText("\"" + inspire + "\"");
+                        btnInspire.setText("More!");
+        }
     }
 }
 
