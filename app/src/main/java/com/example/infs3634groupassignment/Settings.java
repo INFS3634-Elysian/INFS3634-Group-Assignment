1ffe268bcedfb2ef7cf1d0814c776de378661953
Sandrama123 <sandra.ma@student.unsw.edu.au>
2020-04-26 12:11:36 +1000

    Minor UI updates

--- app/src/main/java/com/example/infs3634groupassignment/Settings.java
+++ app/src/main/java/com/example/infs3634groupassignment/Settings.java
@@ -58,11 +58,9 @@ public class Settings extends AppCompatActivity {
                 finalText.setVisibility(View.VISIBLE);
                 finalText.setText("About the App");
                 divider.setVisibility(View.VISIBLE);
-                String text2 =  ("XYZ aims to add value to the wider UNSW community by providing a <b>one-stop-shop solution</b> that prepares them for <b>every step of the recruitment process</b>. Whilst the recruitment process differs across firms and industries, the scope of XYZ will ensure <b>complete coverage of all possible stages</b>." +
-                        "<br />" +
-                        "This will begin from the initial application stage which requires a submission of a resume and cover letter, to all types of psychometric tests, and to the student’s first day of work." +
-                        "<br />" +
-                        "With the founders being UNSW students who have gone through the same process, the <b>personal touch to the application</b> coupled with the use of <b>external resources from industry professionals</b>, we hope to provide UNSW students with a uniquely tailored application that facilitates learning through well-developed resources, tests and other functions"
+                String text2 =  ("JobSecure aims to add value to the wider UNSW community by providing a one-stop-shop solution that prepares them for every step of the recruitment process. Whilst the recruitment process differs across firms and industries in the corporate sector, the scope of JobSecure will ensure complete coverage of all possible stages." +
+                        "<br /><br />" +
+                        "With all founders of Elysian being UNSW students who have gone through the same process, we truly hope that other UNSW students can benefit from our uniquely tailored application that facilitates learning through well-developed resources, tests and other noteworthy features."
                 );
                 finalText2.setText(Html.fromHtml(text2));
                            }
@@ -75,12 +73,12 @@ public class Settings extends AppCompatActivity {
                 finalText.setVisibility(View.VISIBLE);
                 finalText.setText("About the Team");
                 divider.setVisibility(View.VISIBLE);
-                String text2 = ("Our team consists of the following four founders who together, designed and developed the application:" +
+                String text2 = ("Elysian consists of the following four founders who together, designed and developed the application:" +
                         "<br /><br /><b>Cathy Liang</b> | Bachelor of Commerce and Bachelor of Information Systems at UNSW" +
                         "<br /><b>Jasen Yu</b> | Bachelor of Commerce and Bachelor of Information Systems at UNSW" +
                         "<br /><b>Sandra Ma</b> | Bachelor of Commerce and Bachelor of Information Systems at UNSW" +
                         "<br /><b>Truman Ng</b> | Bachelor of Commerce and Bachelor of Information Systems at UNSW" +
-                        "<br /><br /> If you'd like to contact our team, please visit our website: elysian.co"
+                        "<br /><br /> If you'd like to contact our team, please visit our official website: elysian.co"
                 );
                 finalText2.setText(Html.fromHtml(text2));
 
